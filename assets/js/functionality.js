bd708e2aa4b1866323832ca26a5de2afa06f6b6f
kicohen <kicohen@andrew.cmu.edu>
2017-04-14 14:52:16 -0400

    Updated navbar.

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -5,3 +5,9 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
