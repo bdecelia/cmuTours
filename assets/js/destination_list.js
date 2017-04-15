6a777a52e4b9676173837d8c07a21300a6bdada1
Bernard Decelian <bdecelia@andrew.cmu.edu>
2017-04-15 17:28:32 -0400

    began destinations

---
+++ assets/js/destination_list.js
@@ -0,0 +1,16 @@
+var destinations = {
+    "dest_1": {
+        "name": "Warner Hall",
+        "description": "Admissions Office",
+        "img_path": "../img/warner.png",
+        "location": (41.121321, -79.432532)
+    }
+}
+
+function listDestinations(n = null) {
+    for (key in destinations) {
+        var eval_string = '<div class="row destination-row"> <div class="col circle-thumb"></div>'
+        document.write('<div class="row">');
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
